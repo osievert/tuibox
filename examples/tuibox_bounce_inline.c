a2552e9df92fcb56fc8c315c9ec954728da3d603
Otto Sievert <1238259+osievert@users.noreply.github.com>
2026-03-29 11:47:24 -0700

    feat: inline tui (not fullscreen)

--- examples/tuibox_bounce_inline.c
+++ examples/tuibox_bounce_inline.c
@@ -4,15 +4,17 @@
 
 #include <math.h>
 #include <signal.h>
+#include <stdio.h>
 
 #include "../tuibox.h"
 
 #define WINDOW_HEIGHT 5
-#define WINDOW_WIDTH 10
+#define WINDOW_WIDTH 20
 #define FRAME_DELAY_US 16000
 
-static volatile sig_atomic_t running     = 1;
-static int                   frame_drawn = 0;
+static volatile sig_atomic_t running = 1;
+
+static ui_t u;
 
 static void stop(int sig)
 {
@@ -20,58 +22,62 @@ static void stop(int sig)
     running = 0;
 }
 
-static void render_frame(const ui_box_t* box)
+static void stop_key(void)
+{
+    running = 0;
+}
+
+static void draw(ui_box_t* b, char* out)
 {
     int x, y;
 
-    if (frame_drawn)
+    out[0] = '\0';
+    for (y = 0; y < b->h; y++)
     {
-        printf("\r\x1b[%dA", WINDOW_HEIGHT - 1);
-    }
-    else
-    {
-        printf("\x1b[?25l");
-    }
-
-    for (y = 0; y < WINDOW_HEIGHT; y++)
-    {
-        for (x = 0; x < WINDOW_WIDTH; x++)
+        for (x = 0; x < b->w; x++)
         {
-            if (x >= box->x && x < box->x + box->w && y >= box->y && y < box->y + box->h)
-            {
-                printf("\x1b[48;2;255;255;255m \x1b[0m");
-            }
-            else
-            {
-                putchar(' ');
-            }
+            strcat(out, "\x1b[48;2;255;255;255m \x1b[0m");
         }
 
-        if (y + 1 < WINDOW_HEIGHT)
+        if (y + 1 < b->h)
         {
-            putchar('\n');
+            strcat(out, "\n");
         }
     }
-
-    fflush(stdout);
-    frame_drawn = 1;
 }
 
 int main(void)
 {
-    ui_box_t box = {0};
+    int box_id;
+    ui_box_t* box;
     double   vx = 0.65, vy = 0.0;
     double   ax = 0.0, ay = 0.08;
     double   px = 4.0, py = 1.0;
 
-    box.w = 2;
-    box.h = 2;
-    box.x = (int)lround(px);
-    box.y = (int)lround(py);
+    printf("Press 'q' ot ctl-c to quit.\n");
+
+    ui_new_inline(0, WINDOW_WIDTH, WINDOW_HEIGHT, &u);
+
+    box_id = ui_add((int)lround(px),
+                    (int)lround(py),
+                    2,
+                    2,
+                    0,
+                    NULL,
+                    0,
+                    draw,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    &u);
+    box = ui_get(box_id, &u);
 
     signal(SIGTERM, stop);
     signal(SIGQUIT, stop);
     signal(SIGINT, stop);
+
+    ui_key("q", stop_key, &u);
 
     while (running)
     {
@@ -86,9 +92,9 @@ int main(void)
             px = 0.0;
             vx *= -1.0;
         }
-        else if (px + box.w > WINDOW_WIDTH)
+        else if (px + box->w > WINDOW_WIDTH)
         {
-            px = WINDOW_WIDTH - box.w;
+            px = WINDOW_WIDTH - box->w;
             vx *= -1.0;
         }
 
@@ -97,23 +103,20 @@ int main(void)
             py = 0.0;
             vy *= -1.0;
         }
-        else if (py + box.h > WINDOW_HEIGHT)
+        else if (py + box->h > WINDOW_HEIGHT)
         {
-            py = WINDOW_HEIGHT - box.h;
+            py = WINDOW_HEIGHT - box->h;
             vy *= -0.92;
         }
 
-        box.x = (int)lround(px);
-        box.y = (int)lround(py);
+        box->x = (int)lround(px);
+        box->y = (int)lround(py);
 
-        render_frame(&box);
+        ui_draw(&u);
         usleep(FRAME_DELAY_US);
     }
 
-    if (frame_drawn)
-    {
-        printf("\x1b[0m\x1b[?25h\n");
-    }
+    ui_free(&u);
 
     return 0;
 }
--- tuibox.h
+++ tuibox.h
@@ -313,11 +313,14 @@ void vec_swap_(char** data, int* length, int* capacity, int memsz, int idx1, int
 
 #define ui_screen(s, u) u->screen = s;u->force = 1
 
-#define ui_center_x(w, u) (((u)->ws.ws_col - w) / 2)
-#define ui_center_y(h, u) (((u)->ws.ws_row - h) / 2)
+#define ui_center_x(w, u) (((u)->viewport_w - (w)) / 2)
+#define ui_center_y(h, u) (((u)->viewport_h - (h)) / 2)
 
 #define UI_CENTER_X -1
 #define UI_CENTER_Y -1
+
+#define UI_MODE_FULLSCREEN 0
+#define UI_MODE_INLINE     1
 
 /* The argument isn't actually necessary here, but it helps with design consistency */
 #define ui_loop(u) char buf[64];int n;while((n=read(STDIN_FILENO, buf, sizeof(buf))) > 0)
@@ -408,7 +411,10 @@ typedef struct ui_t
     vec_evt_t      e;
     ui_box_t*      click;
     int            mouse, screen, scroll, canscroll, id, force;
+    int            mode, viewport_w, viewport_h, frame_drawn, drawing;
 } ui_t;
+
+void ui_draw(ui_t* u);
 
 /* =========================== */
 
@@ -419,7 +425,7 @@ typedef struct ui_t
  *   necessary escape codes
  *   for mouse support.
  */
-void ui_new(int s, ui_t* u)
+void _ui_new(int s, int mode, int w, int h, ui_t* u)
 {
     struct termios raw;
 
@@ -435,17 +441,40 @@ void ui_new(int s, ui_t* u)
 
     u->click = NULL;
 
-    printf("\x1b[?1049h\x1b[0m\x1b[2J\x1b[?1003h\x1b[?1015h\x1b[?1006h\x1b[?25l");
+    if (mode == UI_MODE_FULLSCREEN)
+    {
+        printf("\x1b[?1049h\x1b[0m\x1b[2J\x1b[?1003h\x1b[?1015h\x1b[?1006h\x1b[?25l");
+    }
+    else
+    {
+        printf("\x1b[?1003h\x1b[?1015h\x1b[?1006h\x1b[?25l");
+    }
 
     u->mouse = 0;
 
     u->screen    = s;
     u->scroll    = 0;
-    u->canscroll = 1;
+    u->canscroll = (mode == UI_MODE_FULLSCREEN);
 
     u->id = 0;
 
     u->force = 0;
+
+    u->mode       = mode;
+    u->viewport_w = (w > 0 ? w : u->ws.ws_col);
+    u->viewport_h = (h > 0 ? h : u->ws.ws_row);
+    u->frame_drawn = 0;
+    u->drawing = 0;
+}
+
+void ui_new(int s, ui_t* u)
+{
+    _ui_new(s, UI_MODE_FULLSCREEN, 0, 0, u);
+}
+
+void ui_new_inline(int s, int w, int h, ui_t* u)
+{
+    _ui_new(s, UI_MODE_INLINE, w, h, u);
 }
 
 /*
@@ -460,7 +489,18 @@ void ui_free(ui_t* u)
     int       i;
     char*     term;
 
-    printf("\x1b[0m\x1b[2J\x1b[?1049l\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?25h");
+    if (u->mode == UI_MODE_FULLSCREEN)
+    {
+        printf("\x1b[0m\x1b[2J\x1b[?1049l\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?25h");
+    }
+    else
+    {
+        printf("\x1b[0m\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?25h");
+        if (u->frame_drawn)
+        {
+            putchar('\n');
+        }
+    }
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &(u->tio));
 
     vec_foreach(&(u->b), val, i)
@@ -546,10 +586,157 @@ void ui_key(char* c, ui_key_func_t f, ui_t* u)
  */
 void ui_clear(ui_t* u)
 {
+    ui_box_t* box;
+    ui_evt_t* evt;
     int tmp = u->screen;
+    int i;
+
+    if (u->mode == UI_MODE_INLINE)
+    {
+        vec_foreach(&(u->b), box, i)
+        {
+            free(box->cache);
+            free(box);
+        }
+        vec_deinit(&(u->b));
+
+        vec_foreach(&(u->e), evt, i)
+        {
+            free(evt);
+        }
+        vec_deinit(&(u->e));
+
+        vec_init(&(u->b));
+        vec_init(&(u->e));
+        u->click = NULL;
+        u->screen = tmp;
+        u->scroll = 0;
+        u->id = 0;
+        u->force = 1;
+        return;
+    }
 
     ui_free(u);
     ui_new(tmp, u);
+}
+
+int _ui_visible_strlen(char* s);
+void _ui_print_clipped(char* s, int start, int end);
+
+void _ui_draw_one_inline(ui_box_t* tmp, ui_t* u)
+{
+    char *buf, *tok;
+    int n = 0;
+    int start_x, end_x;
+
+    if (tmp->screen != u->screen)
+        return;
+
+    buf = (char*)calloc(1, strlen(tmp->cache) * 2);
+    if (u->force || tmp->watch == NULL || *(tmp->watch) != tmp->last)
+    {
+        tmp->draw(tmp, buf);
+        if (tmp->watch != NULL)
+            tmp->last = *(tmp->watch);
+        strcpy(tmp->cache, buf);
+    }
+    else
+    {
+        /* buf is allocated proportionally to tmp->cache, so strcpy is safe */
+        strcpy(buf, tmp->cache);
+    }
+
+    tok = strtok(buf, "\n");
+    while (tok != NULL)
+    {
+        start_x = (tmp->x < 0 ? 0 : tmp->x);
+        end_x = tmp->x + _ui_visible_strlen(tok);
+        if (end_x > u->viewport_w)
+            end_x = u->viewport_w;
+
+        if (start_x < end_x && tmp->y + n >= 0 && tmp->y + n < u->viewport_h)
+        {
+            printf("\x1b[u");
+            if (tmp->y + n > 0)
+                printf("\x1b[%iB", tmp->y + n);
+            if (start_x > 0)
+                printf("\x1b[%iC", start_x);
+            _ui_print_clipped(tok, start_x - tmp->x, end_x - tmp->x);
+        }
+        tok = strtok(NULL, "\n");
+        n++;
+    }
+
+    free(buf);
+}
+
+int _ui_visible_strlen(char* s)
+{
+    int n = 0;
+
+    while (*s != '\0')
+    {
+        if (*s == '\x1b')
+        {
+            s++;
+            if (*s == '[')
+            {
+                s++;
+                while (*s != '\0' && ((*s >= '0' && *s <= '9') || *s == ';' || *s == '?' || *s == '<' || *s == '=' || *s == '>'))
+                {
+                    s++;
+                }
+            }
+            if (*s != '\0')
+            {
+                s++;
+            }
+        }
+        else
+        {
+            n++;
+            s++;
+        }
+    }
+
+    return n;
+}
+
+void _ui_print_clipped(char* s, int start, int end)
+{
+    int col = 0;
+    char* esc;
+
+    while (*s != '\0')
+    {
+        if (*s == '\x1b')
+        {
+            esc = s;
+            s++;
+            if (*s == '[')
+            {
+                s++;
+                while (*s != '\0' && ((*s >= '0' && *s <= '9') || *s == ';' || *s == '?' || *s == '<' || *s == '=' || *s == '>'))
+                {
+                    s++;
+                }
+            }
+            if (*s != '\0')
+            {
+                s++;
+            }
+            fwrite(esc, 1, s - esc, stdout);
+        }
+        else
+        {
+            if (col >= start && col < end)
+            {
+                putchar(*s);
+            }
+            col++;
+            s++;
+        }
+    }
 }
 
 /*
@@ -560,6 +747,21 @@ void ui_draw_one(ui_box_t* tmp, int flush, ui_t* u)
 {
     char *buf, *tok;
     int   n = -1;
+
+    if (u->mode == UI_MODE_INLINE)
+    {
+        if (u->drawing)
+        {
+            _ui_draw_one_inline(tmp, u);
+            if (flush)
+                fflush(stdout);
+        }
+        else
+        {
+            ui_draw(u);
+        }
+        return;
+    }
 
     if (tmp->screen != u->screen)
         return;
@@ -580,9 +782,10 @@ void ui_draw_one(ui_box_t* tmp, int flush, ui_t* u)
     tok = strtok(buf, "\n");
     while (tok != NULL)
     {
-        if (tmp->x > 0 && tmp->x < u->ws.ws_col && CURSOR_Y(tmp) > 0 && CURSOR_Y(tmp) < u->ws.ws_row)
+        if (tmp->x > 0 && tmp->x <= u->ws.ws_col && CURSOR_Y(tmp) > 0 && CURSOR_Y(tmp) <= u->ws.ws_row)
         {
-            printf("\x1b[%i;%iH%s", CURSOR_Y(tmp), tmp->x, tok);
+            printf("\x1b[%i;%iH", CURSOR_Y(tmp), tmp->x);
+            _ui_print_clipped(tok, 0, u->ws.ws_col - tmp->x + 1);
             n++;
         }
         tok = strtok(NULL, "\n");
@@ -600,6 +803,46 @@ void ui_draw(ui_t* u)
 {
     ui_box_t* tmp;
     int       i;
+
+    if (u->mode == UI_MODE_INLINE)
+    {
+        if (u->frame_drawn)
+        {
+            printf("\r");
+            if (u->viewport_h > 1)
+                printf("\x1b[%iA", u->viewport_h - 1);
+        }
+
+        for (i = 0; i < u->viewport_h; i++)
+        {
+            printf("\x1b[0m");
+            printf("%*s", u->viewport_w, "");
+            if (i + 1 < u->viewport_h)
+                putchar('\n');
+        }
+
+        printf("\r");
+        if (u->viewport_h > 1)
+            printf("\x1b[%iA", u->viewport_h - 1);
+        printf("\x1b[s");
+
+        u->drawing = 1;
+        vec_foreach(&(u->b), tmp, i)
+        {
+            _ui_draw_one_inline(tmp, u);
+        }
+        u->drawing = 0;
+
+        printf("\x1b[u");
+        if (u->viewport_h > 1)
+            printf("\x1b[%iB", u->viewport_h - 1);
+        printf("\r");
+
+        fflush(stdout);
+        u->force = 0;
+        u->frame_drawn = 1;
+        return;
+    }
 
     printf("\x1b[0m\x1b[2J");
 
@@ -664,7 +907,10 @@ void _ui_update(char* c, int n, ui_t* u)
                 if (u->canscroll)
                 {
                     u->scroll += (4 * (tok[1] == '4')) - 2;
-                    printf("\x1b[0m\x1b[2J");
+                    if (u->mode == UI_MODE_FULLSCREEN)
+                    {
+                        printf("\x1b[0m\x1b[2J");
+                    }
                     ui_draw(u);
                 }
                 break;
